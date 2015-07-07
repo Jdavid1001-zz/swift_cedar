1a570cdf418c45649875a90d0a98e730da53e480
Juan David Dominguez and Tim Jarratt <jdominguez@pivotal.io>
2015-07-07 14:10:44 -0700

    Invoke CDRRunSpecs in our app delegate
    Might be better to try using the CedarApplicationDelegate.

--- Specs/SpecsAppDelegate.swift
+++ Specs/SpecsAppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Cedar
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        CDRRunSpecs();
         return true
     }
     
